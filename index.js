c38f9d04b62d41b73cf5e51ad61aff51ee86eb34
Zach Lankton <zachlankton@gmail.com>
2022-07-22 16:24:40 +0000

    update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
-var app = require('express')();
+let app = require('express')();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var wSpace;
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 // Add Middleware necessary for REST API's
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,27 +12,25 @@ app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 app.get('/', (req, res) => {
+  console.log("GET /", req.socket.remoteAddress)
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/test', (req, res) => {
-    console.log(req.body);
-  wSpace.emit("ws message", req.body);
-  res.send("thanks!");
-});
+// Dynamic Namespaces
+// The namespace that a client connects with is automatically created
+const namespaces = io.of(/^\/\w+$/);
 
-
-const workspaces = io.of(/^\/\w+$/);
-
-workspaces.on('connection', socket => {
+namespaces.on('connection', socket => {
+  console.log("New Connection", socket.id)
   const workspace = socket.nsp;
   wSpace = workspace;
 
   socket.on("ws message", (msg) => {
+    console.log("New Message on", socket.id)
     workspace.emit("ws message", msg);
   });
 
 });
 
-
+console.log("Starting Server @ port 80")
 http.listen(80, () => {});
